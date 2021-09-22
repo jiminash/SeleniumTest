7ff42ab6d1f8bf2f7aff45b8f5afa5c56a880e27
ashjimin <ashwithajagadesh@gmail.com>
2021-09-22 19:35:23 +0530

    1st commit as ashjimin

--- Selenium/src/main/java/com/SeleniumTest/Selenium/BaseClass.java
+++ Selenium/src/main/java/com/SeleniumTest/Selenium/BaseClass.java
@@ -20,8 +20,8 @@ public class BaseClass {
 		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		
 		driver.get("https://thebankofnewglarus.bank/onlinebankingdemo.html");
 				
