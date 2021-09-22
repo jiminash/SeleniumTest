d5d67777523c4fed0f57b6411337159fca848b2b
ashjimin <ashwithajagadesh@gmail.com>
2021-09-22 19:58:23 +0530

    scscdvdvfbf

--- Selenium/src/main/java/com/SeleniumTest/Selenium/BaseClass.java
+++ Selenium/src/main/java/com/SeleniumTest/Selenium/BaseClass.java
@@ -21,7 +21,7 @@ public class BaseClass {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		
 		driver.get("https://thebankofnewglarus.bank/onlinebankingdemo.html");
 				
